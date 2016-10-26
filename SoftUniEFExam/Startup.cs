0e001c29569936992bbb67b1e5f4c06811d9dc9b
Svetoslav Todorov <drnerf95@gmail.com>
2016-10-26 20:05:56 +0300

    Solution created.

---
+++ SoftUniEFExam/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SoftUniEFExam.Startup))]
+namespace SoftUniEFExam
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
